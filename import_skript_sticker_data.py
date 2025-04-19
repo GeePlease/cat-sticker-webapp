9a6580e1c267ae1de70c512edb64c078a14b0b5d
GeePlease <Flyhigh@gmx.de>
2025-04-19 21:44:30 +0200

    Update import_skript_sticker_data.py
    Finished import script for inserting cat stickers data into the database . Not tested yet.

--- import_skript_sticker_data.py
+++ import_skript_sticker_data.py
@@ -4,7 +4,7 @@ import sqlite3
 import pandas as pd
 import os
 
-# mit SQLite-Datenbank katzensticker.db verbinden und Zugangspunkt
+# mit SQLite-Datenbank katzensticker.db verbinden und Zugangspunkt schaffen
 
 conn = sqlite3.connect('katzensticker.db')
 cur = conn.cursor()
@@ -17,3 +17,36 @@ df = pd.read_csv("C:/Users/peppe/OneDrive/Desktop/Projects & Art/Katzensticker D
 # Die ersten 5 Zeilen der Tabelle anzeigen
 
 print(df.head())  
+
+# Durch jede Zeile der CSV-Datei iterieren und die Daten in die Datenbank einfügen
+
+for index, row in df.iterrows()
+	bild_pfad = row['StickerBild']   # PNG-Bildpfad
+	name = row['Name']				 # Sticker-Name
+	emotion = row['Emotion']		 # Sticker-Emotion
+	aktion = row['Aktion']			 # Sticker-Aktion
+
+	# Fehlerbehandlung, Falls Fehler beim Einfügen auftritt
+	try:
+		# PNG-Bilddatei im binären Format öffnen und lesen
+		with open(bild_pfad,'rb') as file:
+			bild_daten = file.read()
+
+		# Daten in Katzensticker-Datenbank einfügen und speichern
+       cur.execute(
+            """
+            INSERT INTO katzensticker (StickerBild,Name,Emotion,Aktion)
+            VALUES (?, ?, ?, ?)
+            """, 
+            (bild_daten, name, emotion, aktion)
+        )
+
+        # Änderungen in der Datenbank speichern
+        conn.commit()
+
+    except Exception as e:
+        print(f"Fehler bei Zeile {index}:{e}")
+        continue  # Fehler ignorieren und mit der nächsten Zeile fortfahren
+
+# Verbindung zur Datenbank schließen
+conn.close()
